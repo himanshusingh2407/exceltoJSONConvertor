b5d9201fd846c1bb7040dbd91e0c5b9720fa06c1
Himanshu Singh <himanshusingh2407@gmail.com>
2016-06-21 17:32:20 +0530

    added dynamic excel and json update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var storage =   multer.diskStorage({
     callback(null, './uploads');
   },
   filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
+    callback(null, 'sample-data.xlsx');
   }
 });
 var upload = multer({ storage : storage}).single('userPhoto');
@@ -15,18 +15,18 @@ app.get('/',function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/api/photo',function(req,res){
+app.post('/uploadExcel',function(req,res){
   upload(req,res,function(err) {
     if(err) {
       return res.end("Error uploading file.");
     }
     res.end("File is uploaded");
     convertExcel = require('excel-as-json').processFile;
-    convertExcel('sample-data.xlsx', 'sample-data.json');
+    convertExcel('./uploads/sample-data.xlsx', 'sample-data.json');
     console.log('Excel converted successfully');
   });
 });
 
 app.listen(3000,function(){
-  console.log("Working on port 3000");
+  console.log("Server running on port 3000");
 });
