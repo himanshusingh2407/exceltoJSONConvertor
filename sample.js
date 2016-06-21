cf82573bc063240dc015e9d3335d4bb9ebdc092d
Himanshu Singh <himanshusingh2407@gmail.com>
2016-06-21 17:23:19 +0530

    added upload feature

--- sample.js
+++ sample.js
@@ -1,3 +1,14 @@
-convertExcel = require('excel-as-json').processFile;
-convertExcel('sample-data.xlsx', 'sample-data.json');
-console.log('Excel converted successfully');
+var RequestClass = function() {
+  // run code here, or...
+  convertExcel = require('excel-as-json').processFile;
+  convertExcel('sample-data.xlsx', 'sample-data.json');
+  console.log('Excel converted successfully');
+};
+
+// ...add a method, which we do in this example:
+RequestClass.prototype.getList = function() {
+  return "My List";
+};
+
+// now expose with module.exports:
+exports.Request = RequestClass;
